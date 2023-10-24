82ec234dc365790ea9dcb6bb70e8c2f3b66a970c
claver <82104865+cla-bit@users.noreply.github.com>
2023-10-24 17:22:40 +0100

    Update views.py

--- loan/views.py
+++ loan/views.py
@@ -1,30 +1,13 @@
-from django.shortcuts import render
-from .forms import LoadApprovalForm
-from .loan_predictor import predict_loan_approval
-import numpy as np
-from django.conf import settings
 import os
 import joblib
+from django.conf import settings
+from django.shortcuts import render
+from .forms import LoadApprovalForm
 
 # Create your views here.
 
-
-model_file_dir = None
-scaler_file_dir = None
-
-try:
-    model_file_dir = os.path.join(settings.PICKLES_DIR_PATH, 'loan_prediction_model.pkl')
-    with open(model_file_dir, 'rb') as model_file:
-        model = joblib.load(model_file)
-except FileNotFoundError as e:
-    print(f"Error: {e}")
-
-try:
-    scaler_file_dir = os.path.join(settings.PICKLES_DIR_PATH, 'loan_scaler.pkl')
-    with open(scaler_file_dir, 'rb') as scaler_file:
-        scaler = joblib.load(scaler_file)
-except FileNotFoundError as e:
-    print(f"Error: {e}")
+# load the trained model
+model = joblib.load(os.path.join(settings.PICKLES_DIR_PATH, 'loan_prediction_model.pkl'))
 
 
 def loan_prediction(request):
@@ -35,14 +18,27 @@ def loan_prediction(request):
             user_input = form.cleaned_data
             print(user_input.keys())
             print(user_input.values())
-            user_input_int = [int(x) for x in user_input.values()]
-            user_input_array = [np.array(user_input_int)]
-            prediction = model.predict(user_input_array)
-            print(prediction[0])
+            user_input_data = [
+                user_input['Gender'],
+                user_input['Married'],
+                user_input['Dependents'],
+                user_input['Education'],
+                user_input['Self_Employed'],
+                user_input['ApplicantIncome'],
+                user_input['CoApplicantIncome'],
+                user_input['LoanAmount'],
+                user_input['Loan_Amount_Term'],
+                user_input['Credit_History'],
+                user_input['Property_Area'],
+            ]
+            # user_input_int = [int(x) for x in user_input.values()]
+            # user_input_array = [np.array(user_input_int)]
+            prediction = model.predict([user_input_data])[0]
+            print(prediction)
 
             # Call the prediction function
             # prediction = predict_loan_approval(user_input)
-            print(f"Prediction: {prediction[0]}")
+            # print(f"Prediction: {prediction[0]}")
             # return render(request, 'result.html', {'prediction': prediction})
     else:
         form = LoadApprovalForm()
